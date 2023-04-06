11d1681c07e38744d232eb92c54413da5d615483
David Francoeur <dfrancoeur04@gmail.com>
2023-04-06 14:14:25 -0400

    Dummy main that reads from the cmd line args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let guess: String = read_one_line();
+
+    println!("You guessed: {guess}");
+}
+
+fn read_one_line() -> String {
+    let mut value = String::new();
+
+    io::stdin()
+        .read_line(&mut value)
+        .expect("Failed to read line");
+    return value;
 }
