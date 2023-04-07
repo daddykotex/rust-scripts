0921933f4447ae546d2335f6597a267a50ab119c
David Francoeur <dfrancoeur04@gmail.com>
2023-04-07 14:33:40 -0400

    Make a simple http call

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use std::collections::HashMap;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -15,4 +16,12 @@ fn main() {
     let args = Args::parse();
 
     println!("KEY = [{}], SECRET = [{}]", args.api_key, args.api_secret);
+    make_request("https://httpbin.org/ip")
+}
+
+fn make_request(uri: &str) {
+    let resp = reqwest::blocking::get(uri)
+        .expect(&format!("{}:{}", "HTTP request failed", uri))
+        .json::<HashMap<String, String>>();
+    println!("{:#?}", resp);
 }
